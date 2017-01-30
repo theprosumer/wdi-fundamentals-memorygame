ad7de372825152c4c06241e92745f50ede67b764
John Favors <JohnnyFavorite@Johns-MacBook-Pro.local>
2017-01-30 02:26:02 -0600

    updated html using js no cards though

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,33 @@
 console.log("JS file is connected to HTML! Woo!")
+//setup cards in the game
 var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
 
-if (cardOne === cardTwo){
+/*if (cardOne === cardTwo){
 	alert('You Found A Match!');
 } else {
 	alert('Sorry, Try Again.');
+}*/
+function createCards() {
+//get element id of game-baord and set to var
+var board = document.getElementById('game-board');
+
+//set function to create empty board
+function createBoard() {
+//create loop that creates html element for each card
+for (var i = 0; i<cards.length; i++) {
+
+//create the cards 
+//give each elem a div and class of card
+var newCardElement = document.createElement ('div');
+	newCardElement.className = "card";
+
+//append ea card to div with game-baord id
+board.appendChild(newCardElement);
 }
+}
+}
+
+//execute createcards function
