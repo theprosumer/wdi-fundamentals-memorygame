9a9fd1f64bfcd472cb4c52f5e5b42cd2f1add068
John Favors <JohnnyFavorite@Johns-MacBook-Pro.local>
2017-01-29 19:28:25 -0600

    started game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,9 @@ var cardOne = "queen"
 var cardTwo = "king"
 var cardThree = "king"
 var cardFour = "queen"
+
+if (cardTwo===cardFour){
+	alert('You Found A Match!');
+}else {
+	alert('Sorry, Try Again.')
+}
